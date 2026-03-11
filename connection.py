83f2f3b363c3f684c9be0fc842d9c23e79150121
ice912 <130240163+ice912@users.noreply.github.com>
2026-03-11 15:19:25 +0900

    Add files via upload

---
+++ connection.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+DATABASE_URL = "sqlite+aiosqlite:///./db.sqlite"
+
+engine = create_async_engine(DATABASE_URL)
+
+AsyncSessionFactory = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+async def get_session():
+    session = AsyncSessionFactory()
+    try:
+        yield session
+    finally:
+        await session.close()
+
